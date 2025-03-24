fb9dff6ab8fa8baf16857db9309676af9a47e787
zeritonik <zaq31337@gmail.com>
2025-03-24 23:43:23 +0300

    Revert "example of usage"
    This reverts commit f75e0410a56d569e29802e755c7ba02fb77301d9.

--- example/UserState.js
+++
@@ -1,8 +0,0 @@
-import { State } from "../build/State.js";
-
-
-export const userState = new class UserState extends State {
-    constructor() {
-        super(undefined);
-    }
-}(); // сделал так чтобы логи были с именем класса
